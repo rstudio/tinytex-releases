0d22821c1641acb992b3bb88c0881e0574596b8d
Yihui Xie <xie@yihui.name>
2020-09-14 21:03:05 -0500

    move the chocolatey spec and scripts from the repo yihui/tinytex to this repo

---
+++ README.md
@@ -0,0 +1,19 @@
+# The Chocolatey package for TinyTeX
+
+<a href="https://yihui.org/tinytex/"><img src="https://yihui.org/images/logo-tinytex.png" alt="tinytex logo" align="right" width="200px" /></a>
+
+The package is published to Chocolatey and can be easily installed. First, you would need to install [Chocolatey Package Manager](https://chocolatey.org/install) if not already.
+
+Next type in the following commands to install TinyTeX:
+
+```powershell
+choco install tinytex
+```
+
+This will install **TinyTeX ** and make the TeX Live package manager, `tlmgr` available on path.
+
+To uninstall TinyTeX, use the command:
+
+```powershell
+choco uninstall tinytex
+```
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir=Get-ToolsLocation
+$url        = "https://github.com/yihui/chocolatey-tinytex/releases/download/v$env:ChocolateyPackageVersion/TinyTeX-1.zip"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = '00000000000000000000000000000000'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Adds to Path
+$statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path add`""
+Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
+
+$files = get-childitem $installDir -include *.exe -recurse
+foreach ($file in $files) {
+  #We are directly adding it to path so no need to generate shims
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+#create a shim for tlmgr.bat https://chocolatey.org/docs/helpers-install-bin-file
+Install-BinFile -Name tlmgr -Path "$toolsDir\TinyTeX\bin\win32\tlmgr.bat"
