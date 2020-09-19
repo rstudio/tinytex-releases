739d390393f1dda2fdab5074c81b1d101dc26c24
Naveen M K <naveen@syrusdark.website>
2020-09-19 20:47:51 +0530

    Remove from uninstall file also

--- choco/tools/chocolateyuninstall.ps1
+++ choco/tools/chocolateyuninstall.ps1
@@ -1,4 +1,3 @@
 $toolsDir=Get-ToolsLocation
 $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path remove`""
 Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
-Uninstall-BinFile -Name tlmgr -Path "$toolsDir\TinyTeX\bin\win32\tlmgr.bat"
