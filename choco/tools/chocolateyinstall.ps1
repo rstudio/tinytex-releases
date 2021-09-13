ce2491ed150bcd6aa5d634bd2ad73eba972707e7
Naveen M K <naveen521kk@gmail.com>
2021-09-13 14:14:05 +0530

    Reinstall user installed packages on update

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop';
-$toolsDir=Get-ToolsLocation
+$toolsDir = Get-ToolsLocation
 $url        = "https://github.com/yihui/tinytex-releases/releases/download/v$($env:ChocolateyPackageVersion)/TinyTeX-1-v$($env:ChocolateyPackageVersion).zip"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,16 +11,38 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
+
+Write-Host "Running tlmgr path add"
 # Adds to Path
 $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path add`""
 Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
 
+
+Write-Host "Updating tlmgr"
 #updates tlmgr
 $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat update --self`""
 Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
+
 
 $files = get-childitem $toolsDir -include *.exe -recurse
 foreach ($file in $files) {
   #We are directly adding it to path so no need to generate shims
   New-Item "$file.ignore" -type file -force | Out-Null
 }
+
+
+Write-Host "Installing Extra Packages"
+if (Test-Path "$toolsDir\tinytex-pkg-installed-$(Get-Date -Format ddMMyyHH).txt") {
+    $tinytex_tmp_file = "$toolsDir\tinytex-pkg-installed-$(Get-Date -Format ddMMyyHH).txt"
+    $env:PATH = "$toolsDir\TinyTeX\bin\win32;$env:PATH"
+    $tinytex_pkg_installed = (tlmgr info --list --only-installed --data name)
+    $tinytex_reinstall = "$env:TMP\installed_old_packages.ps1"
+    ((Get-Content $tinytex_tmp_file) | Where { $tinytex_pkg_installed -NotContains $_}) | % {"Invoke-Expression '$toolsDir\TinyTeX\bin\win32\tlmgr.bat install {0}'" -f $_} > $tinytex_reinstall
+    if ((Get-Content $tinytex_reinstall) -ne $null){
+        Write-Host "Installing previous user installed packages"
+        Invoke-Expression "$tinytex_reinstall"    
+    }
+    Remove-Item -force $tinytex_reinstall
+    Remove-Item -force $tinytex_tmp_file
+}
+
--- choco/tools/chocolateyuninstall.ps1
+++ choco/tools/chocolateyuninstall.ps1
@@ -1,3 +1,5 @@
-$toolsDir=Get-ToolsLocation
+$toolsDir = Get-ToolsLocation
+
 $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path remove`""
 Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
+
