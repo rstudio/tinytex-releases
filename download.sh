c9bbe08168f8205a884d521b67189a311dc7ceed
Yihui Xie <xie@yihui.name>
2021-11-19 13:22:23 -0600

    build texlive-full on AppVeyor instead and publish it with the gh command

--- download.sh
+++ download.sh
@@ -4,6 +4,7 @@ if [ ! -z $CURRENT_TAG ]; then
   CURRENT_TAG="-v${CURRENT_TAG}"
 fi
 
+# download prebuilt TinyTeX binaries
 for EXT in zip tgz tar.gz
 do
   for BASE in '' -0 -1
@@ -13,3 +14,11 @@ do
   wget -O tinitex.${EXT} https://yihui.org/tinytex/tinitex.${EXT}
 done
 wget -O installer-unix${CURRENT_TAG}.tar.gz https://yihui.org/tinytex/installer-unix.tar.gz
+
+# build texlive-full
+if [ -z $CURRENT_TAG ]; then
+  tar xzf TinyTeX.tar.gz
+  ./.TinyTeX/bin/*/tlmgr install scheme-full
+  tar zcf TinyTeX-2.tar.gz .TinyTeX
+  rm -r .TinyTeX
+fi
