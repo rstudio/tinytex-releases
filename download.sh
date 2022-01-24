bc8c1342f5bf478448b8ce51e3ac02d21034bd28
Yihui Xie <xie@yihui.name>
2022-01-24 13:25:56 -0600

    no need to download the TinyTeX binaries when publishing the daily release (the binaries have been uploaded from the yihui/tinytex repo)

--- download.sh
+++ download.sh
@@ -2,24 +2,22 @@
 
 if [ ! -z $CURRENT_TAG ]; then
   CURRENT_TAG="-v${CURRENT_TAG}"
-fi
-
-# download prebuilt TinyTeX binaries
-for EXT in zip tgz tar.gz
-do
-  for BASE in '' -0 -1
+  # download prebuilt TinyTeX binaries
+  for EXT in zip tgz tar.gz
   do
-    wget -O TinyTeX${BASE}${CURRENT_TAG}.${EXT} https://yihui.org/tinytex/TinyTeX${BASE}.${EXT}
+    for BASE in '' -0 -1
+    do
+      wget -O TinyTeX${BASE}${CURRENT_TAG}.${EXT} https://yihui.org/tinytex/TinyTeX${BASE}.${EXT}
+    done
+    wget -O tinitex.${EXT} https://yihui.org/tinytex/tinitex.${EXT}
   done
-  wget -O tinitex.${EXT} https://yihui.org/tinytex/tinitex.${EXT}
-done
-wget -O installer-unix${CURRENT_TAG}.tar.gz https://yihui.org/tinytex/installer-unix.tar.gz
-
-# build texlive-full
-if [ -z $CURRENT_TAG ]; then
+  wget -O installer-unix${CURRENT_TAG}.tar.gz https://yihui.org/tinytex/installer-unix.tar.gz
+else
+  # build texlive-full
+  wget https://yihui.org/tinytex/TinyTeX.tar.gz
   tar xzf TinyTeX.tar.gz
   ./.TinyTeX/bin/*/tlmgr install scheme-full
   tar zcf TinyTeX-2.tar.gz .TinyTeX
   ls -lh TinyTeX*
-  rm -r .TinyTeX
+  rm -r .TinyTeX TinyTeX.tar.gz
 fi
