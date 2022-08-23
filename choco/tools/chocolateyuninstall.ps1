51d4ca691031ef65b7ce9aa651f630bee41edf3d
Andri <105773745+andrihitz@users.noreply.github.com>
2022-08-23 13:12:03 +0200

    added remove from systemvars

--- choco/tools/chocolateyuninstall.ps1
+++ choco/tools/chocolateyuninstall.ps1
@@ -3,3 +3,5 @@ $toolsDir = Get-ToolsLocation
 $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path remove`""
 Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
 
+$statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path --w32mode=admin remove`""
+Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
