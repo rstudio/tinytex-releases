9cd9e8673947c54a4b844ac5feafd3bd68d7f7ac
Andri <105773745+andrihitz@users.noreply.github.com>
2022-08-30 10:09:54 +0200

    check path to remove tlmgr from sysvars

--- choco/tools/chocolateyuninstall.ps1
+++ choco/tools/chocolateyuninstall.ps1
@@ -1,7 +1,9 @@
 $toolsDir = Get-ToolsLocation
 
-$statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path remove`""
-Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
+If ([Environment]::GetEnvironmentVariables("User").Path -split ";" | ?{$_ -like "*TinyTeX\*"}){
+    $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path remove`""
+} Else{
+    $statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path --w32mode=admin remove`""
+}
 
-$statementsToRun = "/C `"$toolsDir\TinyTeX\bin\win32\tlmgr.bat path --w32mode=admin remove`""
 Start-ChocolateyProcessAsAdmin $statementsToRun "cmd.exe"
